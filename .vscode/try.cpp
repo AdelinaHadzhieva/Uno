70c23e8b851f63a2ff570a45ff4fa7be19d99a4b
AdelinaHadzhieva <anhadzhiev@uni-sofia.bg>
2026-01-06 21:04:28 +0200

    first commit

---
+++ .vscode/try.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int n=0;
+    std::cin >> n;
+    std::cout << "You entered: " << n << std::endl;
+    return 0;
+}
